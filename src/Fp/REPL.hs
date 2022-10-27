3f76616532ad85ef36cd18ea5777c4b011da08f9
Joona Piirainen <joona.piirainen@gmail.com>
2022-10-27 15:55:23 +0300

    feat: add lex and parse commands for REPL

--- src/Fp/REPL.hs
+++ src/Fp/REPL.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE RecordWildCards #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
 -- | This module contains the implementation of the @fp repl@ subcommand
 module Fp.REPL (
@@ -15,14 +16,18 @@ import Control.Monad.State (MonadState (..))
 import Fp.Interpret (Input (..))
 import System.Console.Haskeline (Interrupt (..))
 import System.Console.Repline (CompleterStyle (..), MultiLine (..), ReplOpts (..))
+import Prelude hiding (lex)
 
 import qualified Control.Monad as Monad
 import qualified Control.Monad.Except as Except
 import qualified Control.Monad.State as State
+import Data.Bifunctor (Bifunctor (bimap))
 import qualified Data.Text as Text
 import qualified Data.Text.IO as Text.IO
 import qualified Fp.Interpret as Interpret
+import qualified Fp.Lexer as Lexer
 import qualified Fp.Normalize as Normalize
+import qualified Fp.Parser as Parser
 import qualified Fp.Pretty as Pretty
 import qualified Fp.Value as Value
 import qualified Fp.Width as Width
@@ -40,6 +45,34 @@ repl = do
   let err e =
         liftIO (Text.IO.hPutStrLn IO.stderr (Text.pack (displayException e)))
 
+  let lex input = do
+        let eitherTokens = Lexer.lex "(input)" (Text.pack input)
+
+        case eitherTokens of
+          Left e -> do
+            err e
+          Right tokens -> do
+            liftIO $ putStrLn "------RAW_TOKENS-----"
+            liftIO (print tokens)
+            liftIO $ putStrLn "------END_TOKENS-----"
+
+  let parse input = do
+        let eitherSyntax = Parser.parse "(input)" (Text.pack input)
+
+        case eitherSyntax of
+          Left e -> do
+            err e
+          Right syntax -> do
+            width <- liftIO Width.getWidth
+            liftIO $ putStrLn "------RAW_SYNTAX-----"
+            liftIO (print syntax)
+            liftIO $ putStrLn "----PRETTY_SYNTAX----"
+            Monad.forM_ syntax \s -> do
+              let ss = bimap (const ()) (const ()) s
+              liftIO $
+                Pretty.renderIO True width IO.stdout (Pretty.pretty ss <> "\n")
+            liftIO $ putStrLn "------END_SYNTAX-----"
+
   let command string = do
         let input = Code "(input)" (Text.pack string)
 
@@ -50,12 +83,10 @@ repl = do
             err e
           Right (values, _) -> do
             Monad.forM_ values \value -> do
-              if Value.shouldShow value
-                then do
-                  let syntax = Normalize.quote [] value
-                  width <- liftIO Width.getWidth
-                  liftIO (Pretty.renderIO True width IO.stdout (Pretty.pretty syntax <> "\n"))
-                else pure ()
+              Monad.when (Value.shouldShow value) do
+                let syntax = Normalize.quote [] value
+                width <- liftIO Width.getWidth
+                liftIO (Pretty.renderIO True width IO.stdout (Pretty.pretty syntax <> "\n"))
 
   let help _string = do
         liftIO
@@ -67,6 +98,10 @@ repl = do
               \    Start a multi-line input. Submit with <Ctrl-D>\n\
               \:let IDENTIFIER = EXPRESSION\n\
               \    Assign an expression to a variable\n\
+              \:lex EXPRESSION\n\
+              \    Run the expression throught Lexer.lex\n\
+              \:parse EXPRESSION\n\
+              \    Run the expression throught Parser.parse\n\
               \:quit\n\
               \    Exit the REPL"
           )
@@ -98,6 +133,8 @@ repl = do
   let options =
         [ ("help", Repline.dontCrash . help)
         , ("let", Repline.dontCrash . assignment)
+        , ("lex", Repline.dontCrash . lex)
+        , ("parse", Repline.dontCrash . parse)
         , -- `paste` is included here for auto-completion purposes only.
           -- `repline`'s `multilineCommand` logic overrides this no-op.
           ("paste", Repline.dontCrash . \_ -> return ())
